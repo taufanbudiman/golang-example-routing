875f29be4087365a05bdd1ce38789322e581a0d4
Taufan Budiman <taufanbudiman@yahoo.com>
2021-05-21 09:20:47 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module web-app
+
+go 1.16
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023 // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+type M map[string]interface{}
+
+func main() {
+	r := echo.New()
+
+	r.GET("/", func(ctx echo.Context) error {
+		data := "Hello from /index"
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/html", func(ctx echo.Context) error {
+		data := "Hello from /html"
+		return ctx.HTML(http.StatusOK, data)
+	})
+
+	r.GET("/index", func(ctx echo.Context) error {
+		return ctx.Redirect(http.StatusTemporaryRedirect, "/")
+	})
+
+	r.GET("/json", func(ctx echo.Context) error {
+		data := M{"Message": "Hello", "Counter": 2}
+		return ctx.JSON(http.StatusOK, data)
+	})
+
+	r.GET("/page1", func(ctx echo.Context) error {
+		name := ctx.QueryParam("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page2/:name", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page3/:name/*", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		message := ctx.Param("*")
+
+		data := fmt.Sprintf("Hello %s, I have message for you: %s", name, message)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.POST("/page4", func(ctx echo.Context) error {
+		name := ctx.FormValue("name")
+		message := ctx.FormValue("message")
+
+		data := fmt.Sprintf(
+			"Hello %s, I have message for you: %s",
+			name,
+			strings.Replace(message, "/", "", 1),
+		)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.Start(":9000")
+}
